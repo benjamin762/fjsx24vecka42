192028b72b39a6356aa82b1dc4ad2eb115981977
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2024-10-20 21:32:33 +0200

    Övning 1-4

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("-- String --");
+
+// console.log("\nÖVNING 1 toUpperCase toLowerCase");
+// let mening = prompt("Ange en mening.")
+// console.log(`Versaler: ${mening.toUpperCase()}
+// Gemener: ${mening.toLowerCase()}`); 
+
+
+// console.log("\nÖVNING 2 charAt slice");
+// let s = "JavaScript";
+// console.log(`Första tecknet: ${s.charAt(0)}`)
+// let ny = s.slice(0, 4);
+// console.log(ny);
+
+
+// console.log("\nÖVNING 3 split join");
+// let listaMedOrd = prompt('Ange en lista med ord separerade med kommatecken, t.ex. "äpple,banan,apelsin".')
+// let array = listaMedOrd.split(',');
+// console.log("Array:", array);
+// let nyStrang = array.join(' ');
+// console.log(nyStrang);
+
+// console.log("\nÖVNING 4 replace includes");
+// let alskar = "Jag älskar Programmering";
+// console.log("Innehåller \"Programmering\"", alskar.includes("Programmering"));
+// alskar.replace("Programmering", "JavaScript");
+// console.log(alskar);
